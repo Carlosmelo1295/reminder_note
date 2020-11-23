88152ba108c5415a9fec4c31753b4e0b6104de56
Tklao <carlosmelo1295@gmail.com>
2020-11-23 12:06:14 -0300

    :hammer:

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const newInputAge = document.getElementById("newInputAge");
 const newInputLastName = document.getElementById("newInputLastName");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (checarRadio) => {
   const about = {
     name: newInputName.value,
     lastName: newInputLastName.value,
     age: newInputAge.value,
   };
+
+  let sexo = document.querySelectorAll("#sexo");
+  let sexoEscolha = null;
+  checarRadio = () => {
+    if (sexo[0].checked) {
+      sexoEscolha = "Masculino";
+    }
+    if (sexo[1].checked) {
+      sexoEscolha = "Feminino";
+    }
+  };
+  checarRadio();
+
   if (about.name + about.age == "" || about.age <= 0) {
     Swal.fire({
       icon: "error",
@@ -18,7 +31,7 @@ btn.addEventListener("click", () => {
     });
   } else {
     Swal.fire(
-      `<br> Nome: ${about.name} <br> Sobrenome: ${about.lastName} <br> Idade: ${about.age}`
+      `<br> Nome: ${about.name} <br> Sobrenome: ${about.lastName} <br> Idade: ${about.age} <br> Sexo: ${sexoEscolha}`
     );
   }
 });
